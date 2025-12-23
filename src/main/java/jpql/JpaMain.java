21868fd95168bbbca7e3ec618e7ac47f48d5d8f8
Yoonho Cha <0119hoya@naver.com>
2025-12-23 21:25:08 +0900

    section 11: jpql 함수(사용자 정의 함수 호출 중심으로)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -20,17 +20,18 @@ public class JpaMain {
             team.setName("teamA");
             em.persist(team);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setType(MemberType.ADMIN);
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select nullif(m.username, '관리자') from Member m";
+            String query = "select function('group_concat', m.username) from Member m";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
