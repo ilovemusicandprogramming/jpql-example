eaf750ee058428db174a10b7e4365c16399f9c58
Yoonho Cha <0119hoya@naver.com>
2025-12-23 16:24:10 +0900

    section 11: 조건식(case 등)2

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -21,7 +21,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername("관리자");
             member.setAge(10);
             member.setTeam(team);
             member.setType(MemberType.ADMIN);
@@ -30,7 +30,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select case when m.age <= 10 then '학생요금' when m.age >= 60 then '경로요금' else '일반요금' end from Member m";
+            String query = "select nullif(m.username, '관리자') from Member m";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
