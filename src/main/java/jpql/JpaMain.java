f58ca051c79087b78c07cd2f3389e93fb45d5aca
Yoonho Cha <0119hoya@naver.com>
2025-12-24 15:21:07 +0900

    section 11: 패치조인 기본1

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -17,28 +17,47 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select t.members.size from Team t";
+            String query = "select m from Member m join fetch m.team";
 
-            Collection result = em.createQuery(query, Collection.class)
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
-            System.out.println("result = " + result);
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+
+                //select m from Member m -> 지연로딩으로 인해 N+1
+                //회원1, 팀A(SQL)
+                //회원2, 팀A(1차 캐시)
+                //회원3, 팀B(SQL)
+                // ...
+                //회원100명 -> N+1
+            }
 
             tx.commit();
         } catch (Exception e) {
